79c71c3c73dd80ee92f614ae3ad916f728a5a654
yaoyao0822 <18156631910@163.com>
2022-07-04 19:53:33 +0800

    “代码提交”

---
+++ wyj-project/src/views/HelloWord.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello-word">
+    欢迎辞页面
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class HelloWord extends Vue {}
+</script>
